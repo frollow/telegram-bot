1ba392fcc8874eb7abfcf7f9295e145a53db7be1
Artem Frolov <Artem.Frolov@kaspersky.com>
2019-12-07 16:00:50 +0300

    small text fix

--- telegram_bot.py
+++ telegram_bot.py
@@ -41,15 +41,38 @@ def user_in_the_list(users):
     return result
 
 
+# def update_stat(message):
+#     user_id = message.json['from']['id']
+#     with open('parser_job.csv', 'r', encoding='utf-8') as file:
+#         reader = csv.DictReader(file)
+#         writer = csv.DictWriter(file)
+#         for row in reader:
+#             if int(user_id) == int(row['telegram_id']):
+#                 if message.text == 'Ищу работу':
+#                     print('ищу работу')
+#                     pass
+#                 elif message.text == 'Ищу сотрудника':
+#                     pass
+#                 elif message.text == 'Предложить услугу':
+#                     pass
+
+
+
 def files_writer(users):
     with open('parser_job.csv', 'a', encoding='utf-8') as file:
         # w - all information will be rewrite in the file every time
         # a  - the new information will be added to the file
         a_pen = csv.writer(file)
-        a_pen.writerow((users['from']['id'], users['from']['first_name'], users['from'][
-            'last_name'],
+        a_pen.writerow((users['from']['id'],
+                        users['from']['username'],
+                        users['from']['first_name'],
+                        users['from']['last_name'],
+                        0,
+                        0,
+                        0,
                         users['from']['is_bot'],
-                        users['date']))
+                        users['date'],
+                        0))
         print('Пользователя добавили в таблицу')
 
 
@@ -92,9 +115,6 @@ def vk_group_posts(hashtag, message):
     posts = []
     posts.clear()
     yesterday = int(time.time()) - 86400
-    if len(datas) == 0:
-        bot.send_message(message.chat.id,
-                         "Опаньки... А объявлений пока нет. Попробуй чуть позже.")
     for data in datas:
         if hashtag in data['text'] and data['date'] > yesterday:
             print('Пост добавлен в список')
@@ -106,7 +126,8 @@ def vk_group_posts(hashtag, message):
     for post in posts:
         text = post['text']
         date = time.strftime("%d.%m.%Y | %H:%M:%S", time.gmtime(post['date']))
-        author = f"https://m.vk.com/id{post['author']}"
+        post_author = post["author"]
+        author = f'https://m.vk.com/id{post_author}'
         full_post = \
             f'''
 {text}
@@ -115,11 +136,9 @@ def vk_group_posts(hashtag, message):
 Автор: {author}
             '''
         bot.send_message(message.chat.id, full_post)
-
-
-# @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-#     bot.reply_to(message, 'Сорри, я не еще знаю как обработать это сообщение.')
+    if len(posts) == 0:
+        bot.send_message(message.chat.id,
+                         "Опаньки... А объявлений пока нет. Попробуй чуть позже.")
 
 
 # tested message to chat
